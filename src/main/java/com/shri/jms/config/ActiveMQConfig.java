c715fcb739a40868d4c5df5a96a03f3f48a15fdf
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-18 05:23:45 +0530

    simple task executor to schedule task asynchronously. closes #3

--- src/main/java/com/shri/jms/config/ActiveMQConfig.java
+++ src/main/java/com/shri/jms/config/ActiveMQConfig.java
@@ -3,11 +3,19 @@ package com.shri.jms.config;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
 import org.apache.activemq.artemis.core.server.ActiveMQServer;
 import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAsync
 @Configuration
 public class ActiveMQConfig {
 
+    @Bean
     public void embeddedServerConfig() throws Exception {
         ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
                 .setPersistenceEnabled(false)
@@ -16,5 +24,10 @@ public class ActiveMQConfig {
                 .addAcceptorConfiguration("invm", "vm://0"));
 
         server.start();
+    }
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
     }
 }
