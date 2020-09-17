524356fbe2037f3b95f3f8a532261755a09d3364
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-18 04:55:59 +0530

    project initialization. closes #1

---
+++ src/main/java/com/shri/jms/ZeeroJmsApplication.java
@@ -0,0 +1,13 @@
+package com.shri.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeeroJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeeroJmsApplication.class, args);
+    }
+
+}
